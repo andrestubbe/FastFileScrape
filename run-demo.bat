c140d2f600c0f4bca1abd5f88850c9f2cff43e92
Andre Stubbe <andrestubbe@proton.me>
2026-06-07 19:19:16 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -1,4 +1,9 @@
 @echo off
+chcp 65001 >nul
+
+echo ⚡ Building Main Project...
+call mvn clean package -DskipTests
+if %ERRORLEVEL% NEQ 0 ( pause & exit /b )
 
 echo [FastFileScrape] Running Demo (via JitPack v0.1.0)...
 call mvn -f examples/Demo/pom.xml compile exec:java
